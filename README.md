318acf80cf6ebb9f7365a7f200108fd590d98fdf
x <lvxiang119@gmail.com>
2016-09-27 16:09:58 +0800

    Update readme

--- README.md
+++ README.md
@@ -2,8 +2,18 @@
 
 这是一个简单的密码检查工具。通过检查你的密码是否在多个应用下重复使用来返回一个评估。
 
-# Support adapters
+
+# Support Adapters
+
+#### Active
 
 - github [github](https://github.com)
-- [TODO] weibo  [新浪微博](http://weibo.com/)
-- [TODO] qyqq   [企业qq邮箱](https://exmail.qq.com)
+
+#### TODO     
+
+- [TODO] qyqq [腾讯企业邮箱](https://www.alipay.com/)
+- [TODO] weibo [新浪微博](https://www.alipay.com/)
+- [TODO] taobao [Taobao](https://www.taobao.com)
+- [TODO] alipay [Alipay](https://www.alipay.com/)
+- [TODO] jd [京东](https://www.jd.com)
+- [TODO] more....
--- adapters/adapter.py
+++ adapters/adapter.py
@@ -20,7 +20,7 @@ class Adapter():
         if checked_status[0]==0:
             self.logger.warn(checked_status[1])
         if checked_status[0]==-1:
-            self.logger.error(checked_status[1])
+            self.logger.info(checked_status[1])
 
     def post(self,post,url=False,headers={}):
         if (url==False):
@@ -39,7 +39,7 @@ class Adapter():
     def _init_logger(self):
 
         logger = logging.getLogger("passme")
-        logging.basicConfig(level=logging.WARNING)
+        logging.basicConfig(level=logging.INFO)
         logger.setLevel(logging.WARN)
 
         # # create console handler and set level to debug
